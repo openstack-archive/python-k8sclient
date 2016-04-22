c1d099e7f215ac2c6f3944dd76d826d7c262dbe7
Davanum Srinivas <davanum@gmail.com>
2016-04-22 14:41:27 -0400

    TLS integration for latest pythonk8sclient
    swagger_client uses PoolManager from urllib3. PoolManager keeps the
    connection_pool_kw passed during the init time and uses the same later
    while initiating HTTPSConnection. Existing implementation instantiates
    RESTClientObject() at import time, without giving an option to pass
    the security parameters, viz. key_file, ca_certs etc. So we needed to
    change the way instantiation/initialization was happening.
    
    (copied from I3b490bbb47eb5f961951708dabe6c1abfbcece4e)
    
    Change-Id: I6df227bcd030cb8c374bc83bb44dfea980c893b3

--- k8sclient/client/rest.py
+++ k8sclient/client/rest.py
@@ -232,56 +232,52 @@ class ApiException(Exception):
 
 class RESTClient(object):
     """
-    A class with all class methods to perform JSON requests.
+    A class with methods to perform JSON requests.
     """
 
-    IMPL = RESTClientObject()
+    def __init__(self, key_file=None, cert_file=None, ca_certs=None):
+        self.IMPL = RESTClientObject(key_file=key_file,
+                                     cert_file=cert_file,
+                                     ca_certs=ca_certs)
 
-    @classmethod
-    def request(cls, *n, **kw):
+    def request(self, *n, **kw):
         """
         Perform a REST request and parse the response.
         """
-        return cls.IMPL.request(*n, **kw)
+        return self.IMPL.request(*n, **kw)
 
-    @classmethod
-    def GET(cls, *n, **kw):
+    def GET(self, *n, **kw):
         """
         Perform a GET request using `RESTClient.request()`.
         """
-        return cls.IMPL.GET(*n, **kw)
+        return self.IMPL.GET(*n, **kw)
 
-    @classmethod
-    def HEAD(cls, *n, **kw):
+    def HEAD(self, *n, **kw):
         """
         Perform a HEAD request using `RESTClient.request()`.
         """
-        return cls.IMPL.GET(*n, **kw)
+        return self.IMPL.GET(*n, **kw)
 
-    @classmethod
-    def POST(cls, *n, **kw):
+    def POST(self, *n, **kw):
         """
         Perform a POST request using `RESTClient.request()`
         """
-        return cls.IMPL.POST(*n, **kw)
+        return self.IMPL.POST(*n, **kw)
 
-    @classmethod
-    def PUT(cls, *n, **kw):
+    def PUT(self, *n, **kw):
         """
         Perform a PUT request using `RESTClient.request()`
         """
-        return cls.IMPL.PUT(*n, **kw)
+        return self.IMPL.PUT(*n, **kw)
 
-    @classmethod
-    def PATCH(cls, *n, **kw):
+    def PATCH(self, *n, **kw):
         """
         Perform a PATCH request using `RESTClient.request()`
         """
-        return cls.IMPL.PATCH(*n, **kw)
+        return self.IMPL.PATCH(*n, **kw)
 
-    @classmethod
-    def DELETE(cls, *n, **kw):
+    def DELETE(self, *n, **kw):
         """
         Perform a DELETE request using `RESTClient.request()`
         """
-        return cls.IMPL.DELETE(*n, **kw)
+        return self.IMPL.DELETE(*n, **kw)
